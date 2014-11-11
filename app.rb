e53fbfbc77b8f10f3870db133049e344c90cfabc
Diego <info@diego-suarez.es>
2014-11-11 01:19:01 +0100

    shift with one position works

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ class CaesarCipher
   def initialize string_to_cipher
     @string_to_cipher = string_to_cipher
   end
+  def shift position
+    position = !position.nil? ? position : 1
+    code = ('a'.ord..'z'.ord).to_a
+    string_shifted = ""
+
+    @string_to_cipher.downcase.split('').each do | char |
+      if char != " "
+        tmp_position = char.ord + position
+        tmp_cahr = code.index(tmp_position)
+        string_shifted += code[tmp_cahr].chr
+      else
+        string_shifted += char
+      end
+    end
+    string_shifted
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,8 +5,16 @@ describe CaesarCipher do
 
   describe "Caesar Cipher" do
     context "Get the string" do
+      let(:caesar) do
+        CaesarCipher.new('string')
+      end
+
       it "show the string" do
-        expect(CaesarCipher.new('string').string_to_cipher).to eq('string')
+        expect(caesar.string_to_cipher).to eq('string')
+      end
+
+      it "shift the string one position" do
+        expect(caesar.shift(1)).to eq('tusjoh')
       end
     end
   end
